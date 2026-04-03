b7d244cc972583a476d788138e1b220bf12e837c
amelia24si-sudo <amelia24si@mahasiswa.pcr.ac.id>
2026-04-03 12:38:00 +0700

    Binding login

--- app/src/main/java/com/example/homeapp/Login.kt
+++ app/src/main/java/com/example/homeapp/Login.kt
@@ -1,6 +1,10 @@
 package com.example.homeapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +22,17 @@ class Login : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+//        val inputUser: EditText = findViewById(R.id.Username)
+//        val inputPassword: EditText = findViewById(R.id.Password)
+//        val login: Button = findViewById(R.id.btnlogin)
+
+        binding.btnlogin.setOnClickListener{
+            val user = binding.Username.text
+            val pass = binding.Password.text
+            val intent = Intent(this, Welcome::class.java)
+            startActivity(intent)
+            Toast.makeText(this, "Anda telah login, selamat datang $user!", Toast.LENGTH_SHORT).show()
         }
     }
 }
